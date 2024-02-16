4867bdd4e187bab56e28b81588a1b419c474a2f1
WlodekM <75479945+WlodekM@users.noreply.github.com>
2024-02-16 13:38:21 +0200

    Update credits.svelte

--- src/routes/credits.svelte
+++ src/routes/credits.svelte
@@ -10,7 +10,10 @@
         <h1>Credits</h1>
         <ul>
             <li>
-                <a target="_blank" href="https://github.com/WlodekM">WlodekM</a> - The creator of this client and (for now) it's only coder <small>SOMEONE PLS CONTRIBUTE AAH</small>
+                <a target="_blank" href="https://github.com/WlodekM">WlodekM</a> - The creator of this client and it's main coder
+            </li>
+            <li>
+                <a target="_blank" href="https://github.com/MDWalters">M. D. Walters</a> - 2 commits 💀
             </li>
             <li>
                 <a target="_blank" href="https://github.com/meower-media-co/Meower-Svelte/tree/develop#contributors">The Meower Svelte team</a> - I "borrowed" some code from them but showier says its fine as long as I credit 🤷‍♂️
