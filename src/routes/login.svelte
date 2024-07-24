5acf2be073e1a6444d30cde4e769ba8db25b5886
WlodekM <wlodzio.maly@gmail.com>
2024-07-24 09:10:51 +0300

    uh yeah...

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,6 +1,7 @@
 <script>
-    import { sendCmd, link, connected, meowerRequest } from "@/lib/clm"
-    import { linkUrl, apiUrl } from "@/lib/urls.js";
+    // import { sendCmd, link, connected, meowerRequest } from "@/lib/clm"
+    // import { linkUrl, apiUrl } from "@/lib/urls.js";
+    import * as apic from "@meower/api-client"
     import { goto } from "@roxi/routify"
     import { isLoggedIn, isGuest, user, authHeader } from "@/lib/stores.js"
     import { onMount } from "svelte"
@@ -8,132 +9,135 @@
     let status = ""
     let passwordShown = false
     $: type = passwordShown ? 'text' : 'password'
+    // async function doLogin(username, password, cb) {
+    //     status = "Logging in"
+    //     localStorage.setItem("meower_savedusername", username);
+    //     localStorage.setItem("meower_savedpassword", password);
+    //     try {
+    //         await meowerRequest({
+    //         "cmd": "direct",
+    //         "val": {
+    //             cmd: "authpswd",
+    //             val: {
+    //                 "username": username,
+    //                 "pswd": password,
+    //             },
+    //         }
+    //     })
+    //     } catch (statusCode) {
+    //         switch (statusCode) {
+    //             case "E:103 | ID not found":
+    //                 status = "Invalid username!";
+    //                 return;
+    //             case "E:025 | Deleted":
+    //                 status = "This account has been deleted!";
+    //                 return;
+    //             case "I:011 | Invalid Password":
+    //                 status = "Invalid password!";
+    //                 return;
+    //             case "E:018 | Account Banned":
+    //                 status = "";
+    //                 return;
+    //             case "E:019 | Illegal characters detected":
+    //                 status =
+    //                     "Usernames must not have spaces or other special characters!";
+    //                 return;
+    //             case "E:106 | Too many requests":
+    //                 status = "Too many requests! Please try again later.";
+    //                 return;
+    //             default:
+    //                 status = `Uncaught  error!`
+    //         }
+    //     }
+    //     let res = await fetch(`${apiUrl}/users/${username}`,
+    //     {
+    //         headers: $authHeader,
+    //     })
+    //     const json = await res.json() ?? {
+    //         name: null,
+    //         flags: 0,
+    //         permissions: 0,
+    //         unread_inbox: false,
+    //         theme: "orange",
+    //         mode: true,
+    //         sfx: true,
+    //         bgm: false,
+    //         bgm_song: 2,
+    //         debug: false,
+    //         hide_blocked_users: false,
+    //         favorited_chats: [],
+    //         embeds_enabled: true,
+    //         pfp_data: 1,
+    //         quote: "",
+    //         ban: {
+    //             state: "None",
+    //             expires: 0,
+    //             reason: "",
+    //         },
+    //         xss: false,
+    //         whitelist_enabled: true,
+    //         layout: {css: ""}
+    //     }
+    //     if(!json.name && json._id) {
+    //         json.name = json._id
+    //     }
+    //     function isJsonString(str) {
+    //         try {
+    //             JSON.parse(str);
+    //         } catch (e) {
+    //             return false;
+    //         }
+    //         return true;
+    //     }
+    //     if(isJsonString(json.layout)) {
+    //         json.layout = JSON.parse(json.layout)
+    //     } else {
+    //         json.layout = {css: ""}
+    //     }
+    //     $user = json
+    //     //@ts-ignore
+    //     if (window.mixins) {
+    //         //@ts-ignore
+    //         if (!window.mixins) window.mixins = []
+    //         //@ts-ignore
+    //         window.mixins.forEach(mixin => {
+    //             if(mixin.type == "onLogin") {
+    //                 mixin.function()
+    //             }
+    //         });
+    //     }
+    //     cb()
+    // }
+    // onMount(()=>{
+    //     if(link.ws.CLOSED || link.ws.CONNECTING) {
+    //         link.once("connected", ()=>{
+    //             if (
+    //                 localStorage.getItem("meower_savedusername") &&
+    //                 localStorage.getItem("meower_savedpassword")
+    //             ) {
+    //                 doLogin(localStorage.getItem("meower_savedusername"), localStorage.getItem("meower_savedpassword"), function() {
+    //                     $isLoggedIn = true
+    //                     status = ""
+    //                     $goto("/")
+    //                 })
+    //             }
+    //         })
+    //     } else {
+    //         if (
+    //             localStorage.getItem("meower_savedusername") &&
+    //             localStorage.getItem("meower_savedpassword")
+    //         ) {
+    //             doLogin(localStorage.getItem("meower_savedusername"), localStorage.getItem("meower_savedpassword"), function() {
+    //                 $isLoggedIn = true
+    //                 status = ""
+    //                 $goto("/")
+    //             })
+    //         }
+    //     }
+    // })
     async function doLogin(username, password, cb) {
-        status = "Logging in"
-        localStorage.setItem("meower_savedusername", username);
-        localStorage.setItem("meower_savedpassword", password);
-        try {
-            await meowerRequest({
-            "cmd": "direct",
-            "val": {
-                cmd: "authpswd",
-                val: {
-                    "username": username,
-                    "pswd": password,
-                },
-            }
-        })
-        } catch (statusCode) {
-            switch (statusCode) {
-                case "E:103 | ID not found":
-                    status = "Invalid username!";
-                    return;
-                case "E:025 | Deleted":
-                    status = "This account has been deleted!";
-                    return;
-                case "I:011 | Invalid Password":
-                    status = "Invalid password!";
-                    return;
-                case "E:018 | Account Banned":
-                    status = "";
-                    return;
-                case "E:019 | Illegal characters detected":
-                    status =
-                        "Usernames must not have spaces or other special characters!";
-                    return;
-                case "E:106 | Too many requests":
-                    status = "Too many requests! Please try again later.";
-                    return;
-                default:
-                    status = `Uncaught  error!`
-            }
-        }
-        let res = await fetch(`${apiUrl}/users/${username}`,
-        {
-            headers: $authHeader,
-        })
-        const json = await res.json() ?? {
-            name: null,
-            flags: 0,
-            permissions: 0,
-            unread_inbox: false,
-            theme: "orange",
-            mode: true,
-            sfx: true,
-            bgm: false,
-            bgm_song: 2,
-            debug: false,
-            hide_blocked_users: false,
-            favorited_chats: [],
-            embeds_enabled: true,
-            pfp_data: 1,
-            quote: "",
-            ban: {
-                state: "None",
-                expires: 0,
-                reason: "",
-            },
-            xss: false,
-            whitelist_enabled: true,
-            layout: {css: ""}
-        }
-        if(!json.name && json._id) {
-            json.name = json._id
-        }
-        function isJsonString(str) {
-            try {
-                JSON.parse(str);
-            } catch (e) {
-                return false;
-            }
-            return true;
-        }
-        if(isJsonString(json.layout)) {
-            json.layout = JSON.parse(json.layout)
-        } else {
-            json.layout = {css: ""}
-        }
-        $user = json
-        //@ts-ignore
-        if (window.mixins) {
-            //@ts-ignore
-            if (!window.mixins) window.mixins = []
-            //@ts-ignore
-            window.mixins.forEach(mixin => {
-                if(mixin.type == "onLogin") {
-                    mixin.function()
-                }
-            });
-        }
-        cb()
+        //TODO - Finish later, when docs begin existing
     }
-    onMount(()=>{
-        if(link.ws.CLOSED || link.ws.CONNECTING) {
-            link.once("connected", ()=>{
-                if (
-                    localStorage.getItem("meower_savedusername") &&
-                    localStorage.getItem("meower_savedpassword")
-                ) {
-                    doLogin(localStorage.getItem("meower_savedusername"), localStorage.getItem("meower_savedpassword"), function() {
-                        $isLoggedIn = true
-                        status = ""
-                        $goto("/")
-                    })
-                }
-            })
-        } else {
-            if (
-                localStorage.getItem("meower_savedusername") &&
-                localStorage.getItem("meower_savedpassword")
-            ) {
-                doLogin(localStorage.getItem("meower_savedusername"), localStorage.getItem("meower_savedpassword"), function() {
-                    $isLoggedIn = true
-                    status = ""
-                    $goto("/")
-                })
-            }
-        }
-    })
 </script>
 
 <div class="center-vert" style="height: 100vh;">
